c713f032521ecbda34012a873f0414e45261672d
Faluoke Aisikaer <15935486+faluoke@users.noreply.github.com>
2020-02-23 20:52:21 -0700

    fixed duplicate skills & added comments

--- src/components/Job.js
+++ src/components/Job.js
@@ -5,6 +5,7 @@ import Skills from "./Skills";
 function Job(props) {
   const totalSkillCount = [];
   const applicantSkillCount = [];
+  // used to get row count for job-name row
   const getFullSkillCount = () => {
     props.applicants.map(applicant => {
       props.skills.map(skill => {
@@ -16,7 +17,7 @@ function Job(props) {
     });
     console.log(applicantSkillCount);
   };
-
+  // used to get row count for applicant-name, email, website & coverletter rows
   const getApplicantSkillCount = () => {
     props.skills.map(skill => {
       if (skill.applicant_id === props.applicants[0].id) {
@@ -27,16 +28,18 @@ function Job(props) {
       return false;
     });
   };
-
+  //calls the two count functions above
   getFullSkillCount();
   getApplicantSkillCount();
+  // creates a clone of the applicants & removes the index 1
+  // because data in index 1 is already shown in job-name class
   const applicantsClone = [...props.applicants];
   const skillsClone = [...props.skills];
   applicantsClone.shift();
-  skillsClone.shift();
 
   return (
     <>
+      {/* displays job-name & first applicant information */}
       <tr className="job-name">
         <td rowSpan={totalSkillCount.length}>{props.jobName}</td>
         <td className="applicant-name" rowSpan={applicantSkillCount.length}>
@@ -49,6 +52,7 @@ function Job(props) {
           {props.applicants[0].website}
         </td>
         <td>
+          {/* gets first skill mapping through skills prop and display on ui */}
           {props.skills
             .map(skill => {
               if (skill.applicant_id === props.applicants[0].id) {
@@ -57,6 +61,7 @@ function Job(props) {
               return false;
             })
             .find(skill => {
+              skillsClone.shift();
               return skill;
             })}
         </td>
@@ -64,10 +69,9 @@ function Job(props) {
           {props.applicants[0].cover_letter}
         </td>
       </tr>
-
+      {/* displays the remaining skill(s) mapping through first applicant */}
       {skillsClone.map(skill => {
         if (skill.applicant_id === props.applicants[0].id) {
-          debugger;
           return (
             <tr>
               <td>{skill.name}</td>
@@ -83,6 +87,7 @@ function Job(props) {
           return (
             <>
               <tr>
+                {/* renders an appicant component */}
                 <Applicant
                   key={applicant.id}
                   id={applicant.id}
@@ -91,9 +96,12 @@ function Job(props) {
                   website={applicant.website}
                   coverLetter={applicant.cover_letter}
                   jobId={props.jobId}
+                  // all skills pertaining to this applicant
+                  // used for table row count
                   skills={props.skills.filter(skill => {
                     return skill.applicant_id === applicant.id;
                   })}
+                  // first skill for applicant
                   skill={props.skills
                     .map(skill => {
                       if (skill.applicant_id === applicant.id) {
@@ -107,6 +115,7 @@ function Job(props) {
                     })}
                 />
               </tr>
+              {/* displays all remaining skills from applicant */}
               {skillsClone.map(skill => {
                 if (skill.applicant_id === applicant.id) {
                   return <Skills key={skill.id} skill={skill} />;
